b8abe5b160b86915e12a584b199a1ef17d58400a
dreamlike-ocean <56702218+dreamlike-ocean@users.noreply.github.com>
2024-02-04 23:07:38 +0800

    Update LettuceVertxEventLoop.kt

--- LettuceVertxEventLoop.kt
+++ LettuceVertxEventLoop.kt
@@ -1,143 +1,70 @@
-package com.example.demo
+package org.example
 
-import io.lettuce.core.ClientOptions
+import io.lettuce.core.ExperimentalLettuceCoroutinesApi
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
-import io.lettuce.core.codec.StringCodec
-import io.lettuce.core.resource.ClientResources
+import io.lettuce.core.api.coroutines
+import io.lettuce.core.codec.ByteArrayCodec
+import io.lettuce.core.output.StatusOutput
+import io.lettuce.core.protocol.CommandArgs
+import io.lettuce.core.protocol.CommandType
 import io.lettuce.core.resource.DefaultClientResources
 import io.lettuce.core.resource.EventLoopGroupProvider
-import io.lettuce.core.resource.NettyCustomizer
-import io.netty.bootstrap.Bootstrap
-import io.netty.channel.*
-import io.netty.util.concurrent.*
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.Context
-import io.vertx.core.DeploymentOptions
-import io.vertx.core.Promise
+import io.lettuce.core.support.AsyncConnectionPoolSupport
+import io.lettuce.core.support.BoundedPoolConfig
+import io.netty.channel.DefaultEventLoopGroup
+import io.netty.channel.EventLoop
+import io.netty.channel.EventLoopGroup
+import io.netty.util.concurrent.DefaultPromise
+import io.netty.util.concurrent.EventExecutorGroup
+import io.netty.util.concurrent.GlobalEventExecutor
+import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.core.impl.ContextInternal
-import io.vertx.core.net.NetClientOptions
-import io.vertx.core.net.impl.VertxEventLoopGroup
-import io.vertx.ext.web.Router
-import io.vertx.kotlin.coroutines.CoroutineVerticle
-import io.vertx.kotlin.coroutines.await
-import io.vertx.kotlin.coroutines.dispatcher
-import io.vertx.redis.client.Redis
-import io.vertx.redis.client.RedisAPI
-import io.vertx.redis.client.RedisOptions
-import io.vertx.sqlclient.Pool
-import io.vertx.sqlclient.PoolOptions
-import io.vertx.sqlclient.SqlConnectOptions
-import jdk.jshell.spi.ExecutionControl.RunException
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.future.await
-import kotlinx.coroutines.launch
-import reactor.core.publisher.toMono
-import java.lang.RuntimeException
-import java.util.*
-import java.util.concurrent.Callable
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
-import java.util.function.Consumer
 
 
-suspend fun main(args : Array<String>) {
+@OptIn(ExperimentalLettuceCoroutinesApi::class)
+fun main(): Unit = runBlocking {
+    val redisURI = RedisURI.create("redis://localhost:6379")
     val vertx = Vertx.vertx()
-    val port = if (args.isEmpty()) {
-        8080
-    }else {
-        args[0].toIntOrNull()?:8080
-    }
-    val pool = Pool.pool(
-        vertx,
-        SqlConnectOptions().setDatabase("petclinic")
-            .setHost("localhost")
-            .setPassword("123456789")
-            .setUser("root")
-            .setPort(3306),
-        PoolOptions().setMaxSize(10)
-    )
-
-    val config = DefaultClientResources.builder()
+    val resources = DefaultClientResources
+        .builder()
         .eventLoopGroupProvider(ContextEventLoopGroupProvider(vertx))
-        .eventExecutorGroup(vertx.nettyEventLoopGroup())
         .build()
-    val redis = RedisClient.create(config, "redis://localhost/0")
-
-    vertx.deployVerticle(
-        { HttpVerticle(port, pool,redis) },
-        DeploymentOptions().setInstances(Runtime.getRuntime().availableProcessors())
-    ).await()
-    println("startup end")
-}
-
-
-class HttpVerticle(val port :Int = 8080,val pool: Pool,val redis: RedisClient) : CoroutineVerticle() {
-
-    override suspend fun start() {
-        val client = redis.connectAsync(StringCodec(), RedisURI.create("redis://localhost/0")).await().async()
-        val vertxClient = RedisAPI.api(Redis.createClient(vertx, "redis://localhost/0"))
-        val router = Router.router(vertx)
-        router
-            .get("/hello")
-            .handler { it ->
-                val context = vertx.orCreateContext
-                CoroutineScope(context.dispatcher()).launch {
-                    //    println("before ${System.identityHashCode(vertx.orCreateContext)}: ${Thread.currentThread()}")
-                    client.get("123")
-                        .whenComplete { t, u ->
-                            println("${Thread.currentThread()}")
+    val client = RedisClient.create(resources, redisURI)
+    val pool = AsyncConnectionPoolSupport.createBoundedObjectPool(
+        {
+            client.connectAsync(ByteArrayCodec(), redisURI)
+                .thenCompose { c, t ->
+                    c.async().get("111".toByteArray())
+                        .thenCompose { s ->
+                            println(String(s))
+                            CompletableFuture.completedFuture(c)
                         }
-                        .await()
-                    //  println("after ${System.identityHashCode(vertx.orCreateContext)}: ${Thread.currentThread()}")
-                    delay(5)
-                    val list = pool.query("select * from owners where id = 1")
-                        .mapping { it.toJson() }
-                        .execute()
-                        .await().toList()
-                    it.json(list)
                 }
-            }
-        router
-            .get("/empty")
-            .handler {
-                it.end("hello")
-            }
-        router
-            .get("/lettuce")
-            .handler {
-                val context = vertx.orCreateContext
-                CoroutineScope(context.dispatcher()).launch {
-                    for (index in 1..10) {
-                        client.get("123").whenComplete { t, u ->
-                            println("${Thread.currentThread()}")
-                        }.await()
-                    }
-                    it.end(Thread.currentThread().toString())
-                }
-            }
+        },
+        BoundedPoolConfig.builder().maxTotal(1)
+            .build()
+    )
+    val connection = pool.acquire().await()
+    val coroutinesCommands = connection.async()
+    println(String(coroutinesCommands.get("111".toByteArray()).await()))
+    println(String(coroutinesCommands.get("111".toByteArray()).await()))
 
-        router.get("/vertx-redis")
-            .handler {
-                val context = vertx.orCreateContext
-                CoroutineScope(context.dispatcher()).launch {
-                    for (index in 1..10) {
-                        vertxClient.get("123").await().toString()
-                    }
-                }
-                it.end(Thread.currentThread().toString())
-            }
-
-        val server = vertx.createHttpServer()
-            .requestHandler(router)
-            .listen(port).await()
-        println("listen on $port")
+    vertx.setTimer(1000) {
+        coroutinesCommands.dispatch(CommandType.SET, StatusOutput(ByteArrayCodec()), CommandArgs(ByteArrayCodec()).)
+        pool.release(connection)
     }
+    val coroutinesCommands1 = pool.acquire().await().async()
+    println(String(coroutinesCommands.get("111".toByteArray()).await()))
+    println(String(coroutinesCommands.get("111".toByteArray()).await()))
 }
-
-class ContextEventLoopGroupProvider(val vertx: Vertx) : EventLoopGroupProvider{
-    override fun <T : EventLoopGroup?> allocate(type: Class<T>?) : T {
+class ContextEventLoopGroupProvider(val vertx: Vertx) : EventLoopGroupProvider {
+    override fun <T : EventLoopGroup> allocate(type: Class<T>?) : T {
         return ContextEventLoopGroup(vertx) as T
     }
 
@@ -147,14 +74,14 @@ class ContextEventLoopGroupProvider(val vertx: Vertx) : EventLoopGroupProvider{
         eventLoopGroup: EventExecutorGroup?,
         quietPeriod: Long,
         timeout: Long,
-        unit: TimeUnit?
-    ): Future<Boolean> {
+        unit: TimeUnit
+    ): io.netty.util.concurrent.Future<Boolean> {
         val promise = DefaultPromise<Boolean>(GlobalEventExecutor.INSTANCE)
         promise.setSuccess(true)
         return promise
     }
 
-    override fun shutdown(quietPeriod: Long, timeout: Long, timeUnit: TimeUnit?): Future<Boolean> {
+    override fun shutdown(quietPeriod: Long, timeout: Long, timeUnit: TimeUnit?): io.netty.util.concurrent.Future<Boolean> {
         val promise = DefaultPromise<Boolean>(GlobalEventExecutor.INSTANCE)
         promise.setSuccess(true)
         return promise
