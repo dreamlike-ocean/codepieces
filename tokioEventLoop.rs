7cb002139e6e954b3d85447312071b9b573c24f4
Mengyang Li <56702218+dreamlike-ocean@users.noreply.github.com>
2025-03-23 13:07:58 +0800

    Update tokioEventLoop.rs

--- tokioEventLoop.rs
+++ tokioEventLoop.rs
@@ -1,38 +1,87 @@
-#[warn(unused_imports, unused_variables)]
 use std::pin::Pin;
 use std::sync::Arc;
-use std::task;
+use std::sync::atomic::AtomicU64;
 use std::time::Duration;
 use std::{future::Future, thread};
 
-use tokio::runtime::{Builder};
+use tokio::runtime::Builder;
 use tokio::sync::mpsc;
 use tokio::task::LocalSet;
 use tokio::time;
 
 struct EventLoop {
-    tasks: mpsc::UnboundedSender<Pin<Box<dyn Future<Output = ()> + 'static + Send>>>,
+    tasks: mpsc::UnboundedSender<Pin<Box<dyn Future<Output = ()> + 'static + Send>>>
+}
+
+struct EventLoopGroup {
+    event_loops: Vec<Arc<EventLoop>>,
+    join_hanles: Vec<thread::JoinHandle<()>>,
+    count: AtomicU64,
+    shutdown_signal: tokio::sync::broadcast::Sender<()>,
+}
+
+impl EventLoopGroup {
+    fn new(count: usize) -> EventLoopGroup {
+        let mut event_loops = Vec::with_capacity(count);
+        let mut join_handles = Vec::with_capacity(count);
+        let (send, _) = tokio::sync::broadcast::channel::<()>(1);
+        for _ in 0..count {
+            let rx = send.subscribe();
+            let (eventloop, joion_handle) = EventLoop::new(rx);
+            event_loops.push(Arc::new(eventloop));
+            join_handles.push(joion_handle);
+        }
+
+        EventLoopGroup {
+            event_loops,
+            count: AtomicU64::new(0),
+            shutdown_signal: send,
+            join_hanles: join_handles,
+        }
+    }
+
+    fn new_actor(&self, task: impl Future<Output = ()> + 'static + Send) {
+        let number = self
+            .count
+            .fetch_add(1, std::sync::atomic::Ordering::Acquire);
+        let len = self.event_loops.len();
+        self.event_loops[number as usize % len].spawn(task);
+    }
+
+    fn shutdown(self) {
+        self.shutdown_signal.send(()).unwrap();
+        for ele in self.join_hanles {
+            ele.join().unwrap();
+        }
+    }
 }
 
 impl EventLoop {
-    fn new() -> Self {
+    fn new(mut r: tokio::sync::broadcast::Receiver<()>) -> (Self, thread::JoinHandle<()>) {
         let (sender, mut receiver) = mpsc::unbounded_channel();
 
-        let rt = Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+        let rt = Builder::new_current_thread().enable_all().build().unwrap();
 
-        let thread = thread::spawn(move || {
+        let join_handle = thread::spawn(move || {
             let local = LocalSet::new();
             local.spawn_local(async move {
-                while let Some(task) = receiver.recv().await {
-                    task.await;
+                loop {
+                    tokio::select! {
+                        Some(task) = receiver.recv() => {
+                            task.await;
+                        }
+                        _ = r.recv() => {
+                            break;
+                        }
+                    }
                 }
+                
             });
+
             rt.block_on(local)
         });
-        EventLoop { tasks: sender }
+
+        (EventLoop { tasks: sender }, join_handle)
     }
 
     fn spawn<F>(&self, task: F)
@@ -44,25 +93,15 @@ impl EventLoop {
 }
 
 fn main() {
-    let event_loop = Arc::new( EventLoop::new());
-    
-    event_loop.clone().spawn(async {
-        println!("Hello from EventLoop!");
-        tokio::task::spawn_local(async {
-            println!("Hello from tokio::task::spawn_local!");
-        });
+    let group = EventLoopGroup::new(1);
+
+    group.new_actor(async {
+        loop {
+            println!("Hello world");
+            time::sleep(Duration::from_secs(1)).await;
+        }
     });
 
-    let other = event_loop.clone();
-    thread::spawn(move || {
-        other.clone().spawn(async {
-            println!("Hello from thread!");
-            tokio::task::spawn_local(async {
-                time::sleep(Duration::from_secs(1)).await;
-                println!("Hello from tokio::task::spawn_local! after sleep");
-            });
-        });
-    });
-
-    thread::sleep(Duration::from_secs(5));
+   std::thread::sleep(Duration::from_secs(1));
+    group.shutdown();
 }
